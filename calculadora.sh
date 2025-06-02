3cb4573bbadbf4bb11cc866f56dc0ff623aa2b3d
Pedro Vitor <pedrovitorpvofficial@gmail.com>
2025-06-01 22:17:06 -0300

    Update calculadora.sh

--- calculadora.sh
+++ calculadora.sh
@@ -5,6 +5,6 @@ echo "O programa está sendo inicializado"
 sudo apt update
 sudo apt install python3
 
-/home/pedro/modulo1/projeto/python/calculadora_inteligente.py
+/home/user/calculadora_python/calculadora_inteligente.py
 
 echo "Obrigado por utilizar nossos serviços"
