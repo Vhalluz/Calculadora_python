1241d013910fa2dd2253048d6c7e6bb458490e4b
Vhalluz <pedrovitorpvofficial@gmail.com>
2025-06-01 21:45:14 -0300

    Create calculadora.sh
    Create Calculadora Python

---
+++ calculadora.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "O programa está sendo inicializado"
+
+sudo apt update
+sudo apt install python3
+
+/home/pedro/modulo1/projeto/python/calculadora_inteligente.py
+
+echo "Obrigado por utilizar nossos serviços"
